d0917a7d2411c35a83bc325c677f3f8b6ba23489
Drdiz <drdiz@mail.ru>
2024-09-23 16:10:11 +0300

    2 task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,10 +1,1 @@
-/**
- * sortStrings - sorts array of string by two criteria "asc" or "desc"
- * @param {string[]} arr - the array of strings
- * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
- * @returns {string[]}
- */
-export function sortStrings(arr, param = 'asc') {
-    let arrSort = arr.slice();   
-    return (param == 'asc') ? arrSort.sort((a, b) => a.localeCompare(b, ['ru-RU', 'en-En'], {caseFirst: 'upper'})) : arrSort.sort((a, b) => b.localeCompare(a, ['ru-RU', 'en-En'], {caseFirst: 'lower'}));    
-}
+gi
