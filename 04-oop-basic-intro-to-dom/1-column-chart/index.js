339a23d3fb30b9a5a8d2ae17b479a93b933c4522
Drdiz <drdiz@mail.ru>
2024-10-01 14:28:13 +0300

    4  tasks v.0

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,59 @@
 export default class ColumnChart {
+    element = '';
+    chartHeight = 50;
 
+    constructor(dataObj) {
+        this.dataObj = dataObj;
+        this.data = dataObj.data;
+        this.label = dataObj.label;
+        this.value = dataObj.value;
+        this.link = dataObj.link;
+
+        this.element = this.render().firstElementChild;
+    };
+
+    chartScale(dataArr) {
+        if (dataArr.length  > 0) {
+        const maxChartValue = Math.max(...dataArr);        
+          dataArr = dataArr.map(e => Math.floor(e * this.chartHeight / maxChartValue));
+        }
+        return dataArr
+    };
+
+
+    linker() {
+        return this.link ? `<a href="${this.link}" class="column-chart__link">View all</a>` : ``;
+    }
+
+    get html() {
+        return `
+        <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">        
+      <div class="column-chart__title"> 
+      Total ${this.label}
+             ${this.linker()} 
+      </div>
+      <div class="column-chart__container">
+        <div data-element="header" class="column-chart__header">${this.value}</div>
+        <div data-element="body" class="column-chart__chart">
+        ${this.chartScale(this.data).map((item) => `<div style="--value: ${item}" data-tooltip=""></div>`).join('')}          
+        </div>
+      </div>
+      </div>`
+    };
+
+    render() {
+        let elemHtmlCode = this.createElement(this.html);
+        this.data.length ? elemHtmlCode.firstElementChild.classList.remove(`column-chart_loading`) : false;
+        return elemHtmlCode;
+    };
+
+    createElement(html) {
+        const div = document.createElement('div');
+        div.innerHTML = html;
+        return div
+    }
+
+    remove() {
+        this.element.remove();
+    }
 }
